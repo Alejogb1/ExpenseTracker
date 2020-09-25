37202403419390f502ddfe790016e6547fe5a031
Alejo <alejogarb25@gmail.com>
2020-09-25 16:19:44 -0300

    .

--- expense-tracker-react/src/components/TransactionList.js
+++ expense-tracker-react/src/components/TransactionList.js
@@ -11,7 +11,7 @@ export const TransactionList = () => {
         <>
             <h3>History</h3>
             <ul  className="list">
-                {transactions.map( transaction => (<Transaction ket={transaction.id} 
+                {transactions.map( transaction => (<Transaction key={transaction.id} 
                 transaction = {transaction}/>))}
             </ul>
         </>
